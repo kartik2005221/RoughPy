0c9c4ec88076328baafb6dd75b7800f4ecdae98b
Kartik <kartik2005221@proton.me>
2024-11-21 07:19:37 +0530

    Create indentation.py

---
+++ 100daysofcode/day6/indentation.py
@@ -0,0 +1,12 @@
+def long_function_name(
+  var_one, var_two, var_three,
+  var_four):
+    print(var_one, var_two, var_three, var_four)
+    return var_one
+
+long_function_name(1,2,3,4)
+
+foo = long_function_name(
+        1, 2,
+    3, 4)
+print(foo)
