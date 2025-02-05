352a4789570ee6b3a857e04e2e509a60e3fbbe4a
Kartik <kartik2005221@proton.me>
2025-02-05 22:20:35 +0530

    05-02-2024

---
+++ ollama trial.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# subprocess.run(["echo", "kartik"])
+subprocess.run(["ping", "google.com"])
+# subprocess.run(["ping", "instagram.com"])
+# subprocess.run(["ollama", "run", "llama3.1"])
+# subprocess.run()
